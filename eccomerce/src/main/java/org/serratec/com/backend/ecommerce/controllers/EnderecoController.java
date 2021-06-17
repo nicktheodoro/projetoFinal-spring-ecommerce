c2288677e0a63bcfaebe5b574d2d4a4e9c45380e
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-06-17 13:00:35 -0300

    deploy ecommerce

--- eccomerce/src/main/java/org/serratec/com/backend/ecommerce/controllers/EnderecoController.java
+++ eccomerce/src/main/java/org/serratec/com/backend/ecommerce/controllers/EnderecoController.java
@@ -13,6 +13,7 @@ import org.serratec.com.backend.ecommerce.services.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
